778891f17c1a6e089bcb3bc26496fffab950e4d8
Jacob Emery <jacob.emery@ibm.com>
2025-12-04 13:24:37 -0800

    add epel and fix ansible install
    Signed-off-by: Jacob Emery <jacob.emery@ibm.com>

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 set -e
+
+# Get RHEL major version (e.g. 8, 9)
+. /etc/os-release
+rhel_major="${VERSION_ID%%.*}"
+
 command -v python3 >/dev/null 2>&1 || sudo dnf install -y python3 python3-pip || sudo yum install -y python3 python3-pip
 command -v pip3 >/dev/null 2>&1 || sudo dnf install -y python3-pip || sudo yum install -y python3-pip
-command -v ansible-playbook >/dev/null 2>&1 || sudo pip3 install ansible
+command -v ansible-playbook >/dev/null 2>&1 || \
+  sudo dnf install -y "https://dl.fedoraproject.org/pub/epel/epel-release-latest-${rhel_major}.noarch.rpm" ansible
