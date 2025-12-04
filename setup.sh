4819b1276bb160e7914929b48710c04373451ac2
Jacob Emery <jacob.emery@ibm.com>
2025-12-04 13:26:24 -0800

    make command one line
    Signed-off-by: Jacob Emery <jacob.emery@ibm.com>

--- setup.sh
+++ setup.sh
@@ -7,5 +7,4 @@ rhel_major="${VERSION_ID%%.*}"
 
 command -v python3 >/dev/null 2>&1 || sudo dnf install -y python3 python3-pip || sudo yum install -y python3 python3-pip
 command -v pip3 >/dev/null 2>&1 || sudo dnf install -y python3-pip || sudo yum install -y python3-pip
-command -v ansible-playbook >/dev/null 2>&1 || \
-  sudo dnf install -y "https://dl.fedoraproject.org/pub/epel/epel-release-latest-${rhel_major}.noarch.rpm" ansible
+command -v ansible-playbook >/dev/null 2>&1 || sudo dnf install -y "https://dl.fedoraproject.org/pub/epel/epel-release-latest-${rhel_major}.noarch.rpm" ansible
